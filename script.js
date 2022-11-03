c1a86a3c808a1a5b585aacb0970a28408536f5b4
alessandroforchin <ale.forchin@gmail.com>
2022-11-03 12:07:36 +0100

    From constructor to Class

--- script.js
+++ script.js
@@ -5,48 +5,52 @@ let bookTitle = document.getElementById('title');
 let bookAuthor = document.getElementById('author');
 let bookPages = document.getElementById('pages');
 
-function Book(title, author, pages) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-}
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 
-function displayBook() {
-    const newBox = document.createElement('div');
-    newBox.classList.add('new-box');
-    bookContainer.appendChild(newBox);
+    displayBook() {
+        const newBox = document.createElement('div');
+        newBox.classList.add('new-box');
+        bookContainer.appendChild(newBox);
 
-    const newLine = document.createElement('p');
-    newLine.classList.add('new-line');
-    newBox.appendChild(newLine);
-    newLine.textContent = `"${bookTitle.value}" written by ${bookAuthor.value} has ${bookPages.value} pages.`;
+        const newLine = document.createElement('p');
+        newLine.classList.add('new-line');
+        newBox.appendChild(newLine);
+        newLine.textContent = `"${bookTitle.value}" written by ${bookAuthor.value} has ${bookPages.value} pages.`;
 
-    const newButtons = document.createElement('div');
-    newButtons.classList.add('new-btn-container');
-    newBox.appendChild(newButtons);
+        const newButtons = document.createElement('div');
+        newButtons.classList.add('new-btn-container');
+        newBox.appendChild(newButtons);
 
-    const readButton = document.createElement('button');
-    readButton.classList.add('read-btn');
-    newButtons.appendChild(readButton);
-    readButton.textContent = 'Read';
-    readButton.addEventListener('click', () => {
-        if (readButton.textContent === 'Read') {
-            readButton.textContent = 'Not Read';
-        } else {
-            readButton.textContent = 'Read';
-        }
-    })
-
-    const removeButton = document.createElement('button')
-    removeButton.classList.add('remove-btn');
-    newButtons.appendChild(removeButton);
-    removeButton.textContent = 'Remove';
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        removeButton.addEventListener('click', () => {
-            bookContainer.removeChild(newBox);
-            return myLibrary.splice(i);
+        const readButton = document.createElement('button');
+        readButton.classList.add('read-btn');
+        newButtons.appendChild(readButton);
+        readButton.textContent = 'Read';
+        readButton.addEventListener('click', () => {
+            if (readButton.textContent === 'Read') {
+                readButton.textContent = 'Not Read';
+            } else {
+                readButton.textContent = 'Read';
+            }
         })
+
+        const removeButton = document.createElement('button')
+        removeButton.classList.add('remove-btn');
+        newButtons.appendChild(removeButton);
+        removeButton.textContent = 'Remove';
+
+        for (let i = 0; i < myLibrary.length; i++) {
+            removeButton.addEventListener('click', () => {
+                bookContainer.removeChild(newBox);
+                return myLibrary.splice(i);
+            })
+        }
+
+        return;
     }
 }
 
@@ -56,9 +60,9 @@ document.getElementById('add').addEventListener('click', () => {
     let pages1 = bookPages.value;
 
     let newBook = new Book(title1, author1, pages1);
-    
+        
     myLibrary.push(newBook);
-    displayBook();
+    newBook.displayBook();
 
     document.querySelector('.popup').style.display = 'none';
 })
